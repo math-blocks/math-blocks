ab349f426d0911b504444380797c305310b12673
Kevin Barabash <kevinb@khanacademy.org>
2019-12-01 15:02:22 -0500

    [step-checker]: move equation checker into equation-checker.js and create separate methods for each operation

--- src/step-checker/step-checker.js
+++ src/step-checker/step-checker.js
@@ -3,9 +3,10 @@ import * as Arithmetic from "./arithmetic.js";
 import * as Semantic from "../semantic.js";
 
 import {isNegative, isSubtraction} from "./arithmetic.js";
+import {primeDecomp, zip} from "./util.js";
 
 import FractionChecker from "./fraction-checker.js";
-import {primeDecomp, zip} from "./util.js";
+import EquationChecker from "./equation-checker.js";
 
 // TODO: have a separate function that checks recursively
 // TODO: provide a rational
@@ -77,9 +78,11 @@ export interface IStepChecker {
 
 class StepChecker implements IStepChecker {
     fractionChecker: FractionChecker;
+    equationChecker: EquationChecker;
 
     constructor() {
         this.fractionChecker = new FractionChecker(this);
+        this.equationChecker = new EquationChecker(this);
     }
 
     /**
@@ -140,119 +143,6 @@ class StepChecker implements IStepChecker {
      */
     equality(as: Semantic.Expression[], bs: Semantic.Expression[]): boolean {
         return as.every(a => bs.some(b => this.checkStep(a, b).equivalent));
-    }
-
-    checkEquationStep(a: Semantic.Expression, b: Semantic.Expression): Result {
-        if (a.type !== "eq" || b.type !== "eq") {
-            return {
-                equivalent: false,
-                reasons: [],
-            };
-        }
-
-        const [lhsA, rhsA] = a.args;
-        const [lhsB, rhsB] = b.args;
-        if (lhsB.type === rhsB.type) {
-            if (lhsB.type === "add" && rhsB.type === "add") {
-                const lhsNewTerms = this.difference(
-                    Arithmetic.getTerms(lhsB),
-                    Arithmetic.getTerms(lhsA),
-                );
-                const rhsNewTerms = this.difference(
-                    Arithmetic.getTerms(rhsB),
-                    Arithmetic.getTerms(rhsA),
-                );
-                const lhsNew = Arithmetic.add(lhsNewTerms);
-                const rhsNew = Arithmetic.add(rhsNewTerms);
-                const {equivalent, reasons} = this.checkStep(lhsNew, rhsNew);
-
-                // TODO: handle adding multiple things to lhs and rhs as the same time
-                // TODO: do we want to enforce that the thing being added is exactly
-                // the same or do we want to allow equivalent expressions?
-                if (equivalent && reasons.length === 0) {
-                    if (
-                        isSubtraction(lhsNewTerms[0]) &&
-                        isSubtraction(rhsNewTerms[0])
-                    ) {
-                        return {
-                            equivalent: true,
-                            reasons: [
-                                {
-                                    message:
-                                        "subtracting the same value from both sides",
-                                    nodes: [],
-                                },
-                            ],
-                        };
-                    }
-                    return {
-                        equivalent: true,
-                        reasons: [
-                            {
-                                message: "adding the same value to both sides",
-                                nodes: [],
-                            },
-                        ],
-                    };
-                }
-            }
-
-            if (lhsB.type === "mul" && rhsB.type === "mul") {
-                const lhsNewFactors = this.difference(
-                    Arithmetic.getFactors(lhsB),
-                    Arithmetic.getFactors(lhsA),
-                );
-                const rhsNewFactors = this.difference(
-                    Arithmetic.getFactors(rhsB),
-                    Arithmetic.getFactors(rhsA),
-                );
-                const {equivalent, reasons} = this.checkStep(
-                    Arithmetic.mul(lhsNewFactors),
-                    Arithmetic.mul(rhsNewFactors),
-                );
-
-                // TODO: do we want to enforce that the thing being added is exactly
-                // the same or do we want to allow equivalent expressions?
-                if (equivalent && reasons.length === 0) {
-                    return {
-                        equivalent: true,
-                        reasons: [
-                            {
-                                message:
-                                    "multiplying both sides by the same value",
-                                nodes: [],
-                            },
-                        ],
-                    };
-                }
-            }
-
-            if (lhsB.type === "div" && rhsB.type === "div") {
-                if (
-                    this.checkStep(lhsA, lhsB.args[0]).equivalent &&
-                    this.checkStep(rhsA, rhsB.args[0]).equivalent
-                ) {
-                    if (this.checkStep(lhsB.args[1], rhsB.args[1]).equivalent) {
-                        return {
-                            equivalent: true,
-                            reasons: [
-                                {
-                                    message:
-                                        "dividing both sides by the same value",
-                                    nodes: [],
-                                },
-                            ],
-                        };
-                    } else {
-                        // TODO: custom error message for this case
-                    }
-                }
-            }
-        }
-        return {
-            equivalent: false,
-            reasons: [],
-        };
     }
 
     addInverse(prev: Semantic.Expression, next: Semantic.Expression): Result {
@@ -818,7 +708,7 @@ class StepChecker implements IStepChecker {
             return result;
         }
 
-        result = this.checkEquationStep(a, b);
+        result = this.equationChecker.checkStep(a, b);
         if (result.equivalent) {
             return result;
         }
