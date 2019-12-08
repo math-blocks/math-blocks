70e33cbaf179c0e55da96809b2052da6919f6b49
Kevin Barabash <kevinb@khanacademy.org>
2019-12-08 13:36:43 -0500

    [step-checker]: add .toMatchSteps() custom matche in IntegerChecker tests

--- src/step-checker/__tests__/integer-checker.test.js
+++ src/step-checker/__tests__/integer-checker.test.js
@@ -1,35 +1,74 @@
 // @flow
-import * as Semantic from "../../semantic.js";
 import print from "../../print.js";
 import {parse} from "../../text/text-parser.js";
 
 import StepChecker from "../step-checker.js";
 
+import type {Result} from "../step-checker.js";
+
 const checker = new StepChecker();
 
-const checkStep = (prev: Semantic.Expression, next: Semantic.Expression) =>
-    checker.checkStep(prev, next);
+const checkStep = (prev: string, next: string) => {
+    return checker.checkStep(parse(prev), parse(next));
+};
+
+expect.extend({
+    toMatchSteps(result: Result, steps: string[]) {
+        const nodes = steps.filter((step, index) => index % 2 == 0);
+        for (let i = 0; i < nodes.length - 1; i++) {
+            if (print(result.reasons[i].nodes[0]) !== nodes[i]) {
+                return {
+                    message: () =>
+                        `expected ${print(
+                            result.reasons[i].nodes[0],
+                        )} to equal ${nodes[i]}`,
+                    pass: false,
+                };
+            }
+            if (print(result.reasons[i].nodes[1]) !== nodes[i + 1]) {
+                return {
+                    message: () =>
+                        `expected ${print(
+                            result.reasons[i].nodes[1],
+                        )} to equal ${nodes[i + 1]}`,
+                    pass: false,
+                };
+            }
+        }
+        const messages = steps.filter((step, index) => index % 2);
+        if (result.reasons.length !== messages.length) {
+            return {
+                message: () =>
+                    `expected ${messages.length} reasons but received ${result.reasons.length}`,
+                pass: false,
+            };
+        }
+        for (let i = 0; i < messages.length; i++) {
+            if (result.reasons[i].message !== messages[i]) {
+                return {
+                    message: () =>
+                        `expected ${messages[i]} but received ${result.reasons[i].message}`,
+                    pass: false,
+                };
+            }
+        }
+        return {
+            message: () => `expected steps not to match`,
+            pass: true,
+        };
+    },
+});
 
 describe("IntegerChecker", () => {
     it("a + -a -> 0", () => {
-        const before = parse("a + -a");
-        const after = parse("0");
-
-        const result = checkStep(before, after);
+        const result = checkStep("a + -a", "0");
 
         expect(result.equivalent).toBe(true);
-        expect(result.reasons.map(reason => reason.message)).toEqual([
-            "adding inverse",
-        ]);
-        expect(print(result.reasons[0].nodes[0])).toEqual("a + -a");
-        expect(print(result.reasons[0].nodes[1])).toEqual("0");
+        expect(result).toMatchSteps(["a + -a", "adding inverse", "0"]);
     });
 
     it("0 -> a + -a", () => {
-        const before = parse("0");
-        const after = parse("a + -a");
-
-        const result = checkStep(before, after);
+        const result = checkStep("0", "a + -a");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -40,10 +79,7 @@ describe("IntegerChecker", () => {
     });
 
     it("a + b + -a + c -> b + c", () => {
-        const before = parse("a + b + -a + c");
-        const after = parse("b + c");
-
-        const result = checkStep(before, after);
+        const result = checkStep("a + b + -a + c", "b + c");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -52,10 +88,7 @@ describe("IntegerChecker", () => {
     });
 
     it("a - b -> a + -b", () => {
-        const before = parse("a - b");
-        const after = parse("a + -b");
-
-        const result = checkStep(before, after);
+        const result = checkStep("a - b", "a + -b");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -64,10 +97,7 @@ describe("IntegerChecker", () => {
     });
 
     it("a + -b -> a - b", () => {
-        const before = parse("a + -b");
-        const after = parse("a - b");
-
-        const result = checkStep(before, after);
+        const result = checkStep("a + -b", "a - b");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -76,10 +106,7 @@ describe("IntegerChecker", () => {
     });
 
     it("a + b - c -> a + b + -c", () => {
-        const before = parse("a + b - c");
-        const after = parse("a + b + -c");
-
-        const result = checkStep(before, after);
+        const result = checkStep("a + b - c", "a + b + -c");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -88,23 +115,20 @@ describe("IntegerChecker", () => {
     });
 
     it("a - b - c -> a + -b + -c", () => {
-        const before = parse("a - b - c");
-        const after = parse("a + -b + -c");
-
-        const result = checkStep(before, after);
+        const result = checkStep("a - b - c", "a + -b + -c");
 
         expect(result.equivalent).toBe(true);
-        expect(result.reasons.map(reason => reason.message)).toEqual([
+        expect(result).toMatchSteps([
+            "a - b - c",
             "subtracting is the same as adding the inverse",
+            "a + -b - c",
             "subtracting is the same as adding the inverse",
+            "a + -b + -c",
         ]);
     });
 
     it("a - b - c -> a - b + -c", () => {
-        const before = parse("a - b - c");
-        const after = parse("a - b + -c");
-
-        const result = checkStep(before, after);
+        const result = checkStep("a - b - c", "a - b + -c");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -113,10 +137,7 @@ describe("IntegerChecker", () => {
     });
 
     it("a - -b -> a + --b -> a + b", () => {
-        const before = parse("a - -b");
-        const after = parse("a + b");
-
-        const result = checkStep(before, after);
+        const result = checkStep("a - -b", "a + b");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -136,16 +157,14 @@ describe("IntegerChecker", () => {
     });
 
     it("a + b -> a + --b -> a - -b", () => {
-        const before = parse("a + b");
-        const after = parse("a - -b");
-
-        const result = checkStep(before, after);
+        const result = checkStep("a + b", "a - -b");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
             "negative of a negative is positive",
             "subtracting is the same as adding the inverse",
         ]);
+        // TODO: update .toMatchSteps() to handle this case
         expect(print(result.reasons[0].nodes[0])).toEqual("b");
         expect(print(result.reasons[0].nodes[1])).toEqual("--b");
         expect(print(result.reasons[1].nodes[0])).toEqual("a + --b");
@@ -153,10 +172,7 @@ describe("IntegerChecker", () => {
     });
 
     it("a - a -> 0", () => {
-        const before = parse("a - a");
-        const after = parse("0");
-
-        const result = checkStep(before, after);
+        const result = checkStep("a - a", "0");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -165,10 +181,7 @@ describe("IntegerChecker", () => {
     });
 
     it("--a -> a", () => {
-        const before = parse("--a");
-        const after = parse("a");
-
-        const result = checkStep(before, after);
+        const result = checkStep("--a", "a");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -177,10 +190,7 @@ describe("IntegerChecker", () => {
     });
 
     it("a -> --a", () => {
-        const before = parse("a");
-        const after = parse("--a");
-
-        const result = checkStep(before, after);
+        const result = checkStep("a", "--a");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -191,10 +201,7 @@ describe("IntegerChecker", () => {
     });
 
     it("----a -> --a", () => {
-        const before = parse("----a");
-        const after = parse("--a");
-
-        const result = checkStep(before, after);
+        const result = checkStep("----a", "--a");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -203,10 +210,7 @@ describe("IntegerChecker", () => {
     });
 
     it("--a -> ----a", () => {
-        const before = parse("--a");
-        const after = parse("----a");
-
-        const result = checkStep(before, after);
+        const result = checkStep("--a", "----a");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -217,44 +221,33 @@ describe("IntegerChecker", () => {
     });
 
     it("----a -> a", () => {
-        const before = parse("----a");
-        const after = parse("a");
-
-        const result = checkStep(before, after);
+        const result = checkStep("----a", "a");
 
         expect(result.equivalent).toBe(true);
-        expect(result.reasons.map(reason => reason.message)).toEqual([
+        expect(result).toMatchSteps([
+            "----a",
             "negative of a negative is positive",
+            "--a",
             "negative of a negative is positive",
+            "a",
         ]);
-        expect(print(result.reasons[0].nodes[0])).toEqual("----a");
-        expect(print(result.reasons[0].nodes[1])).toEqual("--a");
-        expect(print(result.reasons[1].nodes[0])).toEqual("--a");
-        expect(print(result.reasons[1].nodes[1])).toEqual("a");
     });
 
     it("a -> ----a", () => {
-        const before = parse("a");
-        const after = parse("----a");
-
-        const result = checkStep(before, after);
+        const result = checkStep("a", "----a");
 
         expect(result.equivalent).toBe(true);
-        expect(result.reasons.map(reason => reason.message)).toEqual([
+        expect(result).toMatchSteps([
+            "a",
             "negative of a negative is positive",
+            "--a",
             "negative of a negative is positive",
+            "----a",
         ]);
-        expect(print(result.reasons[0].nodes[0])).toEqual("a");
-        expect(print(result.reasons[0].nodes[1])).toEqual("--a");
-        expect(print(result.reasons[1].nodes[0])).toEqual("--a");
-        expect(print(result.reasons[1].nodes[1])).toEqual("----a");
     });
 
     it("-a -> -1 * a", () => {
-        const before = parse("-a");
-        const after = parse("-1 * a");
-
-        const result = checkStep(before, after);
+        const result = checkStep("-a", "-1 * a");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -263,10 +256,7 @@ describe("IntegerChecker", () => {
     });
 
     it("-1*a -> -a", () => {
-        const before = parse("-1 * a");
-        const after = parse("-a");
-
-        const result = checkStep(before, after);
+        const result = checkStep("-1 * a", "-a");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -275,10 +265,7 @@ describe("IntegerChecker", () => {
     });
 
     it("(-a)(-b) -> ab", () => {
-        const before = parse("(-a)(-b)");
-        const after = parse("ab");
-
-        const result = checkStep(before, after);
+        const result = checkStep("(-a)(-b)", "ab");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -287,10 +274,7 @@ describe("IntegerChecker", () => {
     });
 
     it("ab -> (-a)(-b)", () => {
-        const before = parse("ab");
-        const after = parse("(-a)(-b)");
-
-        const result = checkStep(before, after);
+        const result = checkStep("ab", "(-a)(-b)");
 
         expect(result.equivalent).toBe(true);
         expect(result.reasons.map(reason => reason.message)).toEqual([
@@ -299,36 +283,28 @@ describe("IntegerChecker", () => {
     });
 
     it("-(a + b) -> -a + -b", () => {
-        const before = parse("-(a + b)");
-        const after = parse("-a + -b");
-
-        const result = checkStep(before, after);
+        const result = checkStep("-(a + b)", "-a + -b");
 
         expect(result.equivalent).toBe(true);
-        expect(result.reasons.map(reason => reason.message)).toEqual([
+        expect(result).toMatchSteps([
+            "-(a + b)",
             "negation is the same as multipling by negative one",
+            "-1 * (a + b)",
             "distribution",
+            "-a + -b",
         ]);
-        expect(print(result.reasons[0].nodes[0])).toEqual("-(a + b)");
-        expect(print(result.reasons[0].nodes[1])).toEqual("-1 * (a + b)");
-        expect(print(result.reasons[1].nodes[0])).toEqual("-1 * (a + b)");
-        expect(print(result.reasons[1].nodes[1])).toEqual("-a + -b");
     });
 
-    it("-a + -b -> -(a + b)", () => {
-        const before = parse("-a + -b");
-        const after = parse("-(a + b)");
-
-        const result = checkStep(before, after);
+    it("-a + -b -> -1 * (a + b) -> -(a + b)", () => {
+        const result = checkStep("-a + -b", "-(a + b)");
 
         expect(result.equivalent).toBe(true);
-        expect(result.reasons.map(reason => reason.message)).toEqual([
+        expect(result).toMatchSteps([
+            "-a + -b",
             "factoring",
+            "-1 * (a + b)",
             "negation is the same as multipling by negative one",
+            "-(a + b)",
         ]);
-        expect(print(result.reasons[0].nodes[0])).toEqual("-a + -b");
-        expect(print(result.reasons[0].nodes[1])).toEqual("-1 * (a + b)");
-        expect(print(result.reasons[1].nodes[0])).toEqual("-1 * (a + b)");
-        expect(print(result.reasons[1].nodes[1])).toEqual("-(a + b)");
     });
 });
