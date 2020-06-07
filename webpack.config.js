bb8b7f62bf0b216fb64580c512ce1132d7d08577
Kevin Barabash <kevinb@khanacademy.org>
2020-06-06 20:03:14 -0400

    reduce coverage requirement to 95%

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,10 @@ module.exports = {
                 __dirname,
                 "./packages/editor/src",
             ),
-            "@math-blocks/metrics": path.join(__dirname, "./packages/metrics/src"),
+            "@math-blocks/metrics": path.join(
+                __dirname,
+                "./packages/metrics/src",
+            ),
             "@math-blocks/react": path.join(__dirname, "./packages/react/src"),
             "@math-blocks/semantic": path.join(
                 __dirname,
