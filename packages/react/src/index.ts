847e6891434b94f3d28749fee44f131eb1f40c88
Kevin Barabash <kevinb@khanacademy.org>
2020-06-30 19:02:05 -0400

    remove old MathRenderer react component

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,7 +1,6 @@
 import Icon from "./icon";
 import MathEditor from "./math-editor";
 import MathKeypad from "./math-keypad";
-import MathRenderer from "./math-renderer";
 import SceneGraphRenderer from "./scene-graph-renderer";
 
-export {Icon, MathEditor, MathKeypad, MathRenderer, SceneGraphRenderer};
+export {Icon, MathEditor, MathKeypad, SceneGraphRenderer};
