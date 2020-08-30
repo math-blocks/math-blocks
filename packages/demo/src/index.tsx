fb345585cff3466bad734443122cf195d3e75c76
Kevin Barabash <kevinb@khanacademy.org>
2020-08-30 00:50:50 -0400

    Initial version of 'handwriting' page to capture handwritten glyphs (#138)
    * Initial version of 'handwriting' page to capture handwritten glyphs
    
    * write tests for distance, clamp, and len
    
    * add new files

--- packages/demo/src/index.tsx
+++ packages/demo/src/index.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {Switch, Route, BrowserRouter as Router} from "react-router-dom";
+import {Link, Switch, Route, BrowserRouter as Router} from "react-router-dom";
 
 import StepCheckerPage from "./step-checker-page";
 import EditorPage from "./editor-page";
 import MathRendererPage from "./math-renderer-page";
+import HandwritingPage from "./handwriting-page";
 
 const container = document.createElement("div");
 
@@ -24,17 +25,23 @@ ReactDOM.render(
             <Route path="/math-renderer">
                 <MathRendererPage />
             </Route>
+            <Route path="/handwriting">
+                <HandwritingPage />
+            </Route>
             <Route path="/">
                 <h1>MathBlocks Demos</h1>
                 <ul>
                     <li>
-                        <a href="/editor">Editor</a>
+                        <Link to="/editor">Editor</Link>
                     </li>
                     <li>
-                        <a href="/math-renderer">Math Renderer</a>
+                        <Link to="/math-renderer">Math Renderer</Link>
                     </li>
                     <li>
-                        <a href="/step-checker">Step Checker</a>
+                        <Link to="/step-checker">Step Checker</Link>
+                    </li>
+                    <li>
+                        <Link to="/handwriting">Handwriting</Link>
                     </li>
                 </ul>
             </Route>
