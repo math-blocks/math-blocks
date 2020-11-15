891c28bf27e8855f8ebac5475570e98e928fec83
Kevin Barabash <kevinb@khanacademy.org>
2020-11-15 12:47:14 -0500

    re-export types using a different syntax

--- packages/step-checker/src/index.ts
+++ packages/step-checker/src/index.ts
@@ -3,6 +3,9 @@ import StepChecker from "./step-checker";
 export {checkStep} from "./step-checker";
 export {MistakeId} from "./enums";
 
-export type {Context, Mistake} from "./types";
+import {Context as _Context, Mistake as _Mistake} from "./types";
+
+export type Context = _Context;
+export type Mistake = _Mistake;
 
 export default StepChecker;
