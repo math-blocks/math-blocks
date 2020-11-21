ab10525ccdb89e7e066612f135f8fcc1163d5661
Kevin Barabash <kevinb@khanacademy.org>
2020-11-21 16:03:03 -0500

    add misisng files

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,7 @@
-const {defaults} = require('jest-config');
-
 module.exports = {
     transform: {
         "^.+\\.[t|j]sx?$": "babel-jest",
     },
-    moduleFileExtensions: [...defaults.moduleFileExtensions, "stories"],
     modulePathIgnorePatterns: ["<rootDir>/out/"],
     moduleNameMapper: {
         "^@math-blocks/(.*)$": "<rootDir>/packages/$1/src/index.ts",
