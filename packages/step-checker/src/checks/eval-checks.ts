a2c254ec32bc11867cce32afa11334a7dd3bab36
Kevin Barabash <kevinb7@gmail.com>
2020-11-28 19:13:27 -0500

    Exclude common terms/factors from evalAdd and evalMul checks, fixes #176 (#186)

--- packages/step-checker/src/checks/eval-checks.ts
+++ packages/step-checker/src/checks/eval-checks.ts
@@ -5,23 +5,31 @@ import * as Semantic from "@math-blocks/semantic";
 import {Options, Check, Correction} from "../types";
 import {MistakeId} from "../enums";
 
-import {correctResult} from "./util";
+import {correctResult, intersection, difference} from "./util";
 
-const parseNode = (
+const evalNode = (
     node: Semantic.Types.Expression,
     options: Options,
 ): Fraction => {
     if (node.type === "number") {
         return new Fraction(node.value);
     } else if (node.type === "neg") {
-        return parseNode(node.arg, options).mul(new Fraction("-1"));
+        return evalNode(node.arg, options).mul(new Fraction("-1"));
     } else if (node.type === "div" && options.evalFractions) {
         // TODO: add a recursive option as well
-        return parseNode(node.args[0], options).div(
-            parseNode(node.args[1], options),
+        return evalNode(node.args[0], options).div(
+            evalNode(node.args[1], options),
         );
-        // TODO: handle evaluating "add" and "mul"
-        // TODO: include substeps when that happens
+    } else if (node.type === "add") {
+        return node.args.reduce(
+            (sum, term) => sum.add(evalNode(term, options)),
+            new Fraction("0"),
+        );
+    } else if (node.type === "mul") {
+        return node.args.reduce(
+            (sum, factor) => sum.mul(evalNode(factor, options)),
+            new Fraction("1"),
+        );
     } else {
         throw new Error(`cannot parse a number from ${node.type} node`);
     }
@@ -46,11 +54,10 @@ export const evalAdd: Check = (prev, next, context) => {
 
     const prevNonNumTerms: Semantic.Types.NumericExpression[] = [];
     const prevNumTerms: Semantic.Types.NumericExpression[] = [];
-    let prevSum = new Fraction("0");
     for (const term of prevTerms) {
         if (Semantic.isNumber(term)) {
             try {
-                prevSum = prevSum.add(parseNode(term, checker.options));
+                evalNode(term, checker.options);
             } catch (e) {
                 return;
             }
@@ -60,17 +67,13 @@ export const evalAdd: Check = (prev, next, context) => {
         }
     }
 
-    // Prevents infinite recursion
-    if (prevSum.equals(0)) {
-        return;
-    }
-
     const nextNumTerms: Semantic.Types.NumericExpression[] = [];
-    let nextSum = new Fraction("0");
     for (const term of nextTerms) {
         if (Semantic.isNumber(term)) {
             try {
-                nextSum = nextSum.add(parseNode(term, checker.options));
+                // TODO: update parseNode to handle all of the cases when
+                // Semantic.isNumber return true.
+                evalNode(term, checker.options);
             } catch (e) {
                 return;
             }
@@ -78,17 +81,40 @@ export const evalAdd: Check = (prev, next, context) => {
         }
     }
 
+    // Find any exact matches between the numeric terms in prev and next and
+    // remove them.
+    const commonTerms = intersection(prevNumTerms, nextNumTerms);
+    const uniquePrevNumTerms = difference(prevNumTerms, commonTerms);
+    const uniqueNextNumTerms = difference(nextNumTerms, commonTerms);
+
     // We don't recognize things like 5 + 3 -> 6 + 2 as a valid step, maybe we should
-    if (nextNumTerms.length >= prevNumTerms.length) {
+    if (uniqueNextNumTerms.length >= uniquePrevNumTerms.length) {
         return;
     }
 
-    // Prevents reporting unhelpful mistakes
-    if (nextSum.equals(0)) {
+    const uniquePrevSum = evalNode(
+        Semantic.addTerms(uniquePrevNumTerms),
+        checker.options,
+    );
+
+    // Prevents infinite recursion
+    if (uniquePrevSum.equals(0)) {
         return;
     }
 
-    if (!prevSum.equals(nextSum)) {
+    const uniqueNextSum = evalNode(
+        Semantic.addTerms(uniqueNextNumTerms),
+        checker.options,
+    );
+
+    // Prevents reporting unhelpful mistakes.  Numeric terms adding to zero is
+    // usually a sign of the addition of inverses, e.g. 5 + -5.  We prefer that
+    // check so we return early here.
+    if (uniqueNextSum.equals(0)) {
+        return;
+    }
+
+    if (!uniquePrevSum.equals(uniqueNextSum)) {
         if (context.mistakes) {
             // Corrections only make sense for EVAL_ADD since with decomposition
             // there are multiple correct decompositions.  Also, we currently
@@ -96,18 +122,22 @@ export const evalAdd: Check = (prev, next, context) => {
             // TODO: make this less restrictive by removing common number from
             // prev and next.
             const corrections: Correction[] = [];
-            if (!context.reversed && nextNumTerms.length === 1) {
+            if (!context.reversed && uniqueNextNumTerms.length === 1) {
                 corrections.push({
-                    id: nextNumTerms[0].id,
-                    replacement: Semantic.number(prevSum.toString()),
+                    id: uniqueNextNumTerms[0].id,
+                    replacement: Semantic.number(uniquePrevSum.toString()),
                 });
             }
             context.mistakes.push({
                 id: context.reversed
                     ? MistakeId.DECOMP_ADD
                     : MistakeId.EVAL_ADD,
-                prevNodes: context.reversed ? nextNumTerms : prevNumTerms,
-                nextNodes: context.reversed ? prevNumTerms : nextNumTerms,
+                prevNodes: context.reversed
+                    ? uniqueNextNumTerms
+                    : uniquePrevNumTerms,
+                nextNodes: context.reversed
+                    ? uniquePrevNumTerms
+                    : uniqueNextNumTerms,
                 corrections: corrections,
             });
         }
@@ -115,6 +145,7 @@ export const evalAdd: Check = (prev, next, context) => {
         return;
     }
 
+    // TODO: check if uniqueNextNumTerms and uniqueNextNumTerms sum to zero
     const newPrev = Semantic.addTerms([...prevNonNumTerms, ...nextNumTerms]);
 
     const result = checker.checkStep(newPrev, next, context);
@@ -152,13 +183,10 @@ export const evalMul: Check = (prev, next, context) => {
 
     const prevNonNumFactors: Semantic.Types.NumericExpression[] = [];
     const prevNumFactors: Semantic.Types.NumericExpression[] = [];
-    let prevProduct = new Fraction("1");
     for (const factor of prevFactors) {
         if (Semantic.isNumber(factor)) {
             try {
-                prevProduct = prevProduct.mul(
-                    parseNode(factor, checker.options),
-                );
+                evalNode(factor, checker.options);
             } catch (e) {
                 return;
             }
@@ -168,19 +196,13 @@ export const evalMul: Check = (prev, next, context) => {
         }
     }
 
-    // Prevents infinite recursion
-    if (prevProduct.equals(1)) {
-        return;
-    }
-
     const nextNumFactors: Semantic.Types.NumericExpression[] = [];
-    let nextProduct = new Fraction("1");
     for (const factor of nextFactors) {
         if (Semantic.isNumber(factor)) {
             try {
-                nextProduct = nextProduct.mul(
-                    parseNode(factor, checker.options),
-                );
+                // TODO: update parseNode to handle all of the cases when
+                // Semantic.isNumber return true.
+                evalNode(factor, checker.options);
             } catch (e) {
                 return;
             }
@@ -188,17 +210,40 @@ export const evalMul: Check = (prev, next, context) => {
         }
     }
 
+    // Find any exact matches between the numeric terms in prev and next and
+    // remove them.
+    const commonFactors = intersection(prevNumFactors, nextNumFactors);
+    const uniquePrevNumFactors = difference(prevNumFactors, commonFactors);
+    const uniqueNextNumFactors = difference(nextNumFactors, commonFactors);
+
     // We don't recognize things like 5 * 3 -> 6 * 2 as a valid step, maybe we should
-    if (nextNumFactors.length >= prevNumFactors.length) {
+    if (uniqueNextNumFactors.length >= uniquePrevNumFactors.length) {
         return;
     }
 
-    // Prevents reporting unhelpful mistakes
-    if (nextProduct.equals(1)) {
+    const uniquePrevProduct = evalNode(
+        Semantic.mulFactors(uniquePrevNumFactors),
+        checker.options,
+    );
+
+    // Prevents infinite recursion
+    if (uniquePrevProduct.equals(1)) {
         return;
     }
 
-    if (!prevProduct.equals(nextProduct)) {
+    const uniqueNextProduct = evalNode(
+        Semantic.mulFactors(uniqueNextNumFactors),
+        checker.options,
+    );
+
+    // Prevents reporting unhelpful mistakes.  Numeric terms multiplying to one
+    // is usually a sign of multiplying of inverses, e.g. 5 * 1/5.  We prefer
+    // that check so we return early here.
+    if (uniqueNextProduct.equals(1)) {
+        return;
+    }
+
+    if (!uniquePrevProduct.equals(uniqueNextProduct)) {
         if (context.mistakes) {
             // Corrections only make sense for EVAL_MUL since with decomposition
             // there are multiple correct decompositions.  Also, we currently
@@ -206,10 +251,10 @@ export const evalMul: Check = (prev, next, context) => {
             // TODO: make this less restrictive by removing common number from
             // prev and next.
             const corrections: Correction[] = [];
-            if (!context.reversed && nextNumFactors.length === 1) {
+            if (!context.reversed && uniqueNextNumFactors.length === 1) {
                 corrections.push({
-                    id: nextNumFactors[0].id,
-                    replacement: Semantic.number(prevProduct.toString()),
+                    id: uniqueNextNumFactors[0].id,
+                    replacement: Semantic.number(uniquePrevProduct.toString()),
                 });
             }
             context.mistakes.push({
@@ -221,8 +266,12 @@ export const evalMul: Check = (prev, next, context) => {
                 id: context.reversed
                     ? MistakeId.DECOMP_MUL
                     : MistakeId.EVAL_MUL,
-                prevNodes: context.reversed ? nextNumFactors : prevNumFactors,
-                nextNodes: context.reversed ? prevNumFactors : nextNumFactors,
+                prevNodes: context.reversed
+                    ? uniqueNextNumFactors
+                    : uniquePrevNumFactors,
+                nextNodes: context.reversed
+                    ? uniquePrevNumFactors
+                    : uniqueNextNumFactors,
                 corrections: corrections,
             });
         }
