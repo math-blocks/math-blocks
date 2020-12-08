4f0fee0e4ad27aee0da16993674e77f368831994
Kevin Barabash <kevinb7@gmail.com>
2020-12-07 22:11:27 -0500

    Add collectLikeTerms check (#212)
    * Add collectLikeTerms check
    
    * add another test case to fix in the future

--- packages/step-checker/src/checks/__tests__/polynomial-checks.test.ts
+++ packages/step-checker/src/checks/__tests__/polynomial-checks.test.ts
@@ -2,26 +2,88 @@ import {checkStep, toHaveMessages} from "../test-util";
 
 expect.extend({toHaveMessages});
 
-describe.skip("polynomial checks", () => {
+describe("polynomial checks", () => {
     it("2x + 3x -> 5x", () => {
         const result = checkStep("2x + 3x", "5x");
 
         expect(result).toBeTruthy();
         expect(result).toHaveMessages([
-            "factoring",
+            "collect like terms",
             "evaluation of addition",
-            "commutative property",
         ]);
     });
 
-    it("1 + 2x + 3x -> 1 + 5x", () => {
-        const result = checkStep("1 + 2x + 3x", "1 + 5x");
+    it("(1/2)(x) + (3/2)(x) -> 2x", () => {
+        const result = checkStep("(1/2)(x) + (3/2)(x)", "2x");
 
         expect(result).toBeTruthy();
         expect(result).toHaveMessages([
-            "factoring",
+            "collect like terms",
             "evaluation of addition",
-            "commutative property",
+        ]);
+    });
+
+    it("5x + -2x -> 3x", () => {
+        const result = checkStep("5x + -2x", "3x");
+
+        expect(result).toBeTruthy();
+        expect(result).toHaveMessages([
+            "collect like terms",
+            "evaluation of addition",
+        ]);
+    });
+
+    it("5x - 2x -> 3x", () => {
+        const result = checkStep("5x - 2x", "3x");
+
+        expect(result).toBeTruthy();
+        expect(result).toHaveMessages([
+            "subtracting is the same as adding the inverse",
+            "move negation inside multiplication",
+            "collect like terms",
+            "evaluation of addition",
+        ]);
+    });
+
+    it("2x + 3x + 4y + 5y -> 5x + 9y", () => {
+        const result = checkStep("2x + 3x + 4y + 5y", "5x + 9y");
+
+        expect(result).toBeTruthy();
+        expect(result).toHaveMessages([
+            "collect like terms",
+            "evaluation of addition",
+            "evaluation of addition",
+        ]);
+    });
+
+    it("1 + 2x + 3x -> 5x + 1", () => {
+        const result = checkStep("1 + 2x + 3x", "5x + 1");
+
+        expect(result).toBeTruthy();
+        expect(result).toHaveMessages([
+            "collect like terms",
+            "evaluation of addition",
+        ]);
+    });
+
+    it("2x + 3x + 1 -> 5x + 1", () => {
+        const result = checkStep("2x + 3x + 1", "5x + 1");
+
+        expect(result).toBeTruthy();
+        expect(result).toHaveMessages([
+            "collect like terms",
+            "evaluation of addition",
+        ]);
+    });
+
+    it("7 + 2x + 3x + 1 -> 5x + 8", () => {
+        const result = checkStep("7 + 2x + 3x + 1", "5x + 8");
+
+        expect(result).toBeTruthy();
+        expect(result).toHaveMessages([
+            "evaluation of addition",
+            "collect like terms",
+            "evaluation of addition",
         ]);
     });
 
@@ -30,9 +92,8 @@ describe.skip("polynomial checks", () => {
 
         expect(result).toBeTruthy();
         expect(result).toHaveMessages([
-            "factoring",
+            "collect like terms",
             "evaluation of addition",
-            "commutative property",
         ]);
     });
 
@@ -41,9 +102,8 @@ describe.skip("polynomial checks", () => {
 
         expect(result).toBeTruthy();
         expect(result).toHaveMessages([
-            "factoring",
+            "collect like terms",
             "evaluation of addition",
-            "commutative property",
         ]);
     });
 
@@ -52,9 +112,40 @@ describe.skip("polynomial checks", () => {
 
         expect(result).toBeTruthy();
         expect(result).toHaveMessages([
-            "factoring",
+            "collect like terms",
             "evaluation of addition",
-            "commutative property",
+        ]);
+    });
+
+    it("x + x -> 2x", () => {
+        const result = checkStep("x + x", "2x");
+
+        expect(result).toBeTruthy();
+        expect(result).toHaveMessages([
+            "collect like terms",
+            "evaluation of addition",
+        ]);
+    });
+
+    // TODO: get this test case passing
+    it.skip("3/x + 4/x -> 7/x", () => {
+        const result = checkStep("3/x + 4/x", "7/x");
+
+        expect(result).toBeTruthy();
+        expect(result).toHaveMessages([
+            "collect like terms",
+            "evaluation of addition",
+        ]);
+    });
+
+    // TODO: get this test case passing
+    it.skip("x/3 + x/4 -> 7x/12", () => {
+        const result = checkStep("x/3 + x/4", "7x/12");
+
+        expect(result).toBeTruthy();
+        expect(result).toHaveMessages([
+            "collect like terms",
+            "evaluation of addition",
         ]);
     });
 });
