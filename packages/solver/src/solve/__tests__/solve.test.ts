51a68a3c02080fee844e220eef185d0aaed1e3d5
Kevin Barabash <kevinb@khanacademy.org>
2020-12-29 22:18:42 -0500

    collect steps in 'solve'

--- packages/solver/src/solve/__tests__/solve.test.ts
+++ packages/solver/src/solve/__tests__/solve.test.ts
@@ -1,122 +1,132 @@
 import * as Semantic from "@math-blocks/semantic";
 import {parse, print} from "@math-blocks/testing";
 
-import {solve} from "../solve";
+import {solve as _solve} from "../solve";
+import {Step} from "../types";
 
-type Node = Semantic.Types.Node;
+const solve = (
+    node: Semantic.Types.Node,
+    ident: Semantic.Types.Ident,
+): Step => {
+    const result = _solve(node, ident);
+    if (!result) {
+        throw new Error("no step returned");
+    }
+    return result;
+};
 
 describe("solve", () => {
     describe("linear equations", () => {
         test("2x + 3x = 7 - 4", () => {
             const ast = parse("2x + 3x = 7 - 4");
 
-            const result: Node = solve(ast, Semantic.identifier("x"));
+            const result = solve(ast, Semantic.identifier("x"));
 
-            expect(print(result)).toEqual("x = 3 / 5");
+            expect(print(result.after)).toEqual("x = 3 / 5");
         });
 
         test("2x = 7 + 3x", () => {
             const ast = parse("2x = 7 + 3x");
 
-            const result: Node = solve(ast, Semantic.identifier("x"));
+            const result = solve(ast, Semantic.identifier("x"));
 
-            expect(print(result)).toEqual("x = -7");
+            expect(print(result.after)).toEqual("x = -7");
         });
 
         test("-x / -1 = -7", () => {
             const ast = parse("-x / -1 = -7");
 
-            const result: Node = solve(ast, Semantic.identifier("x"));
+            const result = solve(ast, Semantic.identifier("x"));
 
-            expect(print(result)).toEqual("x = -7");
+            expect(print(result.after)).toEqual("x = -7");
         });
 
         test("7 + 3x = 2x", () => {
             const ast = parse("7 + 3x = 2x");
 
-            const result: Node = solve(ast, Semantic.identifier("x"));
+            const result = solve(ast, Semantic.identifier("x"));
 
-            expect(print(result)).toEqual("x = -7");
+            expect(print(result.after)).toEqual("x = -7");
         });
 
         test("2x + 5 = 7 + 3x", () => {
             const ast = parse("2x + 5 = 7 + 3x");
 
-            const result: Node = solve(ast, Semantic.identifier("x"));
+            const result = solve(ast, Semantic.identifier("x"));
 
-            expect(print(result)).toEqual("x = -2");
+            expect(print(result.after)).toEqual("x = -2");
         });
 
         test("2x + 1 = 7", () => {
             const ast = parse("2x + 1 = 7");
 
-            const result: Node = solve(ast, Semantic.identifier("x"));
+            const result = solve(ast, Semantic.identifier("x"));
 
-            expect(print(result)).toEqual("x = 3");
+            expect(print(result.after)).toEqual("x = 3");
         });
 
         test("7 = 2x + 1", () => {
             const ast = parse("7 = 2x + 1");
 
-            const result: Node = solve(ast, Semantic.identifier("x"));
+            const result = solve(ast, Semantic.identifier("x"));
 
-            expect(print(result)).toEqual("3 = x");
+            expect(print(result.after)).toEqual("3 = x");
         });
 
         test("x + 1 = -2x + 5", () => {
             const ast = parse("x + 1 = -2x + 5");
 
-            const result: Node = solve(ast, Semantic.identifier("x"));
+            const result = solve(ast, Semantic.identifier("x"));
 
-            expect(print(result)).toEqual("x = 4 / 3");
+            expect(print(result.after)).toEqual("x = 4 / 3");
         });
 
         test("-x + 1 = -2x + 5", () => {
             const ast = parse("-x + 1 = -2x + 5");
 
-            const result: Node = solve(ast, Semantic.identifier("x"));
+            const result = solve(ast, Semantic.identifier("x"));
 
-            expect(print(result)).toEqual("x = 4");
+            expect(print(result.after)).toEqual("x = 4");
         });
 
         test("2 - x = 5", () => {
             const ast = parse("2 - x = 5");
 
-            const result: Node = solve(ast, Semantic.identifier("x"));
+            const result = solve(ast, Semantic.identifier("x"));
 
-            expect(print(result)).toEqual("x = -3");
+            expect(print(result.after)).toEqual("x = -3");
         });
 
         test("2 - 2x = 5", () => {
             const ast = parse("2 - 2x = 5");
 
-            const result: Node = solve(ast, Semantic.identifier("x"));
+            const result = solve(ast, Semantic.identifier("x"));
 
-            expect(print(result)).toEqual("x = -(3 / 2)");
+            expect(print(result.after)).toEqual("x = -(3 / 2)");
         });
 
         test("2 - x = 5 - 3x", () => {
             const ast = parse("2 - x = 5 - 3x");
 
-            const result: Node = solve(ast, Semantic.identifier("x"));
+            const result = solve(ast, Semantic.identifier("x"));
 
-            expect(print(result)).toEqual("x = 3 / 2");
+            expect(print(result.after)).toEqual("x = 3 / 2");
         });
 
         test("-x + 3x = 3", () => {
             const ast = parse("-x + 3x = 3");
 
-            const result: Node = solve(ast, Semantic.identifier("x"));
+            const result = solve(ast, Semantic.identifier("x"));
 
-            expect(print(result)).toEqual("x = 3 / 2");
+            expect(print(result.after)).toEqual("x = 3 / 2");
         });
 
         test("2x + 3 = 3", () => {
             const ast = parse("2x + 3 = 3");
 
-            const result: Node = solve(ast, Semantic.identifier("x"));
+            const result = solve(ast, Semantic.identifier("x"));
 
-            expect(print(result)).toEqual("x = 0");
+            expect(print(result.after)).toEqual("x = 0");
         });
     });
 });
--- packages/solver/src/solve/solve.ts
+++ packages/solver/src/solve/solve.ts
@@ -3,14 +3,24 @@ import * as Semantic from "@math-blocks/semantic";
 import {divBothSides} from "./transforms/div-both-sides";
 import {moveTermsToOneSide} from "./transforms/move-terms-to-one-side";
 import {simplifyBothSides} from "./transforms/simplify-both-sides";
-import {Transform} from "./types";
+import {Step, Transform} from "./types";
 
-export const solve = (
-    node: Semantic.Types.Node,
-    ident: Semantic.Types.Ident,
-): Semantic.Types.Node => {
+/**
+ * Solve an equation for a given variable.
+ *
+ * Handles the following types of equations:
+ * - linear
+ *
+ * TODO:
+ * - linear inequality
+ * - quadratic
+ *
+ * @param node the equation (or system of equations) being solved
+ * @param ident the variable being solved for
+ */
+export const solve: Transform = (node, ident) => {
     if (node.type !== "eq") {
-        return node;
+        return undefined;
     }
 
     const transforms: Transform[] = [
@@ -19,27 +29,31 @@ export const solve = (
         divBothSides,
     ];
 
+    const substeps: Step[] = [];
     let current = node as Semantic.Types.Node;
-    for (const transform of transforms) {
-        const next = transform(current, ident);
-        if (next) {
-            current = next.after;
+    for (let i = 0; i < 10; i++) {
+        let changed = false;
+        for (const transform of transforms) {
+            const next = transform(current, ident);
+            if (next) {
+                changed = true;
+                current = next.after;
+                substeps.push(next);
+            }
+        }
+        if (!changed) {
+            break;
         }
     }
 
-    for (const transform of transforms) {
-        const next = transform(current, ident);
-        if (next) {
-            current = next.after;
-        }
+    if (substeps.length > 0) {
+        return {
+            message: "solve for variable", // TODO: include variable in message
+            before: node,
+            after: current,
+            substeps,
+        };
     }
 
-    for (const transform of transforms) {
-        const next = transform(current, ident);
-        if (next) {
-            current = next.after;
-        }
-    }
-
-    return current;
+    return undefined;
 };
