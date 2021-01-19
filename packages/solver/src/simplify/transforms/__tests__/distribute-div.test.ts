9f48fca91b12ffbbf1c618068bd5f9d317ca6280
Kevin Barabash <kevinb@khanacademy.org>
2021-01-18 22:43:04 -0500

    add missing files

---
+++ packages/solver/src/simplify/transforms/__tests__/distribute-div.test.ts
@@ -0,0 +1,55 @@
+import * as Semantic from "@math-blocks/semantic";
+import * as Testing from "@math-blocks/testing";
+
+import {distributeDiv as _distributeDiv} from "../distribute-div";
+import {Step} from "../../types";
+
+const distributeDiv = (node: Semantic.types.Node): Step => {
+    const result = _distributeDiv(node, []);
+    if (!result) {
+        throw new Error("no step returned");
+    }
+    return result;
+};
+
+describe("distribute division", () => {
+    test("(a + b) / c", () => {
+        const ast = Testing.parse("(a + b) / c");
+
+        const step = distributeDiv(ast);
+
+        expect(step.message).toEqual("distribute division");
+        expect(Testing.print(step.after)).toEqual("a / c + b / c");
+        expect(step.substeps.map((substep) => substep.message)).toEqual([]);
+    });
+
+    test("(a - b) / c", () => {
+        const ast = Testing.parse("(a - b) / c");
+
+        const step = distributeDiv(ast);
+
+        expect(step.message).toEqual("distribute division");
+        expect(Testing.print(step.after)).toEqual("a / c - b / c");
+        expect(step.substeps.map((substep) => substep.message)).toEqual([]);
+    });
+
+    test("(a + b) / -c", () => {
+        const ast = Testing.parse("(a + b) / -c");
+
+        const step = distributeDiv(ast);
+
+        expect(step.message).toEqual("distribute division");
+        expect(Testing.print(step.after)).toEqual("-(a / c) - b / c");
+        expect(step.substeps.map((substep) => substep.message)).toEqual([]);
+    });
+
+    test("(a - b) / -c", () => {
+        const ast = Testing.parse("(a - b) / -c");
+
+        const step = distributeDiv(ast);
+
+        expect(step.message).toEqual("distribute division");
+        expect(Testing.print(step.after)).toEqual("-(a / c) + b / c");
+        expect(step.substeps.map((substep) => substep.message)).toEqual([]);
+    });
+});
