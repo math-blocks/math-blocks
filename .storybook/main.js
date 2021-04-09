3d1a23d98e28a0e85e95c1c0aacb954af74a426b
Kevin Barabash <kevinb@khanacademy.org>
2021-04-09 16:53:35 -0400

    Upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,8 @@ const path = require("path");
 
 module.exports = {
     stories: [
-        "../**/stories/**/*.stories.mdx",
-        "../**/stories/**/*.stories.@(ts|tsx)",
+        "../packages/react/src/stories/*.stories.mdx",
+        "../packages/react/src/stories/*.stories.@(ts|tsx)",
     ],
     addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
     presets: [path.resolve(__dirname, "./aliases-preset.js")],
