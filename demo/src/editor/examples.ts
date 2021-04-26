7b8894f4768962af29be84da46ade9dcf252712c
Kevin Barabash <kevinb7@gmail.com>
2021-04-26 09:44:13 -0400

    Add example select dropdown to editor page (#371)

---
+++ demo/src/editor/examples.ts
@@ -0,0 +1,117 @@
+import * as Editor from "@math-blocks/editor-core";
+
+const simpleRow = Editor.util.row("2x+5=10");
+
+const delimiters = Editor.builders.row([
+    Editor.builders.glyph("x"),
+    Editor.builders.glyph("+"),
+    Editor.builders.delimited(
+        [
+            Editor.builders.frac(
+                [
+                    Editor.builders.glyph("y"),
+                    Editor.builders.glyph("\u2212"),
+                    Editor.builders.glyph("1"),
+                ],
+                [Editor.builders.glyph("x")],
+            ),
+        ],
+        Editor.builders.glyph("("),
+        Editor.builders.glyph(")"),
+    ),
+    Editor.builders.subsup(
+        [Editor.builders.glyph("n")],
+        [Editor.builders.glyph("2")],
+    ),
+    Editor.builders.glyph("+"),
+    Editor.builders.glyph("z"),
+]);
+
+const allNodeTypes = Editor.builders.row([
+    Editor.builders.glyph("2"),
+    Editor.builders.glyph("+"),
+    Editor.builders.frac(
+        [Editor.builders.glyph("1")],
+        [
+            Editor.builders.root(
+                [
+                    Editor.builders.glyph("1"),
+                    Editor.builders.glyph("2"),
+                    Editor.builders.glyph("3"),
+                ],
+                [
+                    Editor.builders.glyph("x"),
+                    Editor.builders.subsup(undefined, [
+                        Editor.builders.glyph("2"),
+                    ]),
+                    Editor.builders.glyph("+"),
+                    Editor.builders.frac(
+                        [Editor.builders.glyph("1")],
+                        [
+                            Editor.builders.glyph("a"),
+                            Editor.builders.subsup(
+                                [Editor.builders.glyph("n")],
+                                undefined,
+                            ),
+                        ],
+                    ),
+                ],
+            ),
+        ],
+    ),
+    Editor.builders.glyph("\u2212"),
+    Editor.builders.glyph("\u2212"),
+    Editor.builders.glyph("y"),
+    Editor.builders.glyph("+"),
+    Editor.builders.limits(
+        Editor.builders.row([
+            Editor.builders.glyph("l"),
+            Editor.builders.glyph("i"),
+            Editor.builders.glyph("m"),
+        ]),
+        [
+            Editor.builders.glyph("x"),
+            Editor.builders.glyph("\u2192"), // \rightarrow
+            Editor.builders.glyph("0"),
+        ],
+    ),
+    Editor.builders.glyph("x"),
+    Editor.builders.glyph("+"),
+    Editor.builders.limits(
+        Editor.builders.glyph("\u2211"), // \sum
+        [
+            Editor.builders.glyph("i"),
+            Editor.builders.glyph("="),
+            Editor.builders.glyph("0"),
+        ],
+        [Editor.builders.glyph("\u221E")], // \infty
+    ),
+    Editor.builders.glyph("i"),
+]);
+
+const nestedFractions = Editor.builders.row([
+    Editor.builders.glyph("a"),
+    Editor.builders.glyph("+"),
+    Editor.builders.frac(
+        [
+            Editor.builders.glyph("2"),
+            Editor.builders.glyph("+"),
+            Editor.builders.frac(
+                [
+                    Editor.builders.glyph("x"),
+                    Editor.builders.glyph("+"),
+                    Editor.builders.glyph("1"),
+                ],
+                [Editor.builders.glyph("1")],
+            ),
+            Editor.builders.glyph("\u2212"),
+            Editor.builders.glyph("\u2212"),
+            Editor.builders.glyph("y"),
+        ],
+        [Editor.builders.glyph("1")],
+    ),
+    Editor.builders.glyph("\u2212"),
+    Editor.builders.glyph("b"),
+]);
+
+export const examples = [simpleRow, allNodeTypes, delimiters, nestedFractions];
