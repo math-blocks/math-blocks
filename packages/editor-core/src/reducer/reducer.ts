41ad99d829f346491cb9cffacf88edee5ebc8488
Kevin Barabash <kevinb@khanacademy.org>
2021-06-06 18:38:40 -0400

    Update Action type be a tagged union with explicit tags

--- packages/editor-core/src/reducer/reducer.ts
+++ packages/editor-core/src/reducer/reducer.ts
@@ -12,7 +12,7 @@ import {color} from "./color";
 import {cancel} from "./cancel";
 
 import {zrow} from "./util";
-import type {Zipper, State} from "./types";
+import type {Zipper, State, Action} from "./types";
 
 const initialZipper: Zipper = {
     row: zrow(getId(), [], []),
@@ -26,64 +26,38 @@ const initialState: State = {
     selecting: false,
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type Action = {type: string; detail?: any};
-
 export const zipperReducer = (
     state: State = initialState,
     action: Action,
 ): State => {
     switch (action.type) {
-        case "ArrowLeft": {
+        case "ArrowLeft":
             return moveLeft(state);
-        }
-        case "ArrowRight": {
+        case "ArrowRight":
             return moveRight(state);
-        }
-        case "Backspace": {
+        case "Backspace":
             return backspace(state);
-        }
-        case "_": {
+        case "Subscript":
             return subsup(state, 0);
-        }
-        case "^": {
+        case "Superscript":
             return subsup(state, 1);
-        }
-        case "(":
-        case ")":
-        case "[":
-        case "]":
-        case "{":
-        case "}": {
-            return parens(state, action.type);
-        }
-        case "/": {
+        case "Parens":
+            return parens(state, action.char);
+        case "Fraction":
             return frac(state);
-        }
         // TODO: use "Sqrt" and "NthRoot" to differentiate the two possibilities
-        case "\u221A": {
+        case "Root":
             return root(state, false);
-        }
-        case "Color": {
-            return color(state, action.detail);
-        }
-        case "Cancel": {
-            return cancel(state, action.detail);
-        }
+        case "Color":
+            return color(state, action.color);
+        // Split this into cancel and uncancel
+        case "Cancel":
+            return cancel(state, action.cancelID);
         // We don't handle any other actions yet so ignore them and return the
         // current startZipper.
-        default: {
-            // We ignore all control characters as well the space character.
-            if (action.type.length === 1 && action.type.charCodeAt(0) > 32) {
-                let char = action.type;
-                if (char === "*") {
-                    char = "\u00B7";
-                } else if (char === "-") {
-                    char = "\u2212";
-                }
-                return insertChar(state, char);
-            }
+        case "InsertChar":
+            return insertChar(state, action.char);
+        default:
             return state;
-        }
     }
 };
