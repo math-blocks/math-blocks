3382da701911f040e7e5f80c0ee9b3564418e038
Kevin Barabash <kevinb@khanacademy.org>
2021-06-12 22:56:05 -0400

    Move typesetAtom and helper functions to their own file

--- packages/typesetter/src/typeset.ts
+++ packages/typesetter/src/typeset.ts
@@ -12,6 +12,7 @@ import {typesetLimits} from "./typesetters/limits";
 import {typesetRoot} from "./typesetters/root";
 import {typesetSubsup} from "./typesetters/subsup";
 import {typesetTable} from "./typesetters/table";
+import {typesetAtom, maybeAddOperatorPadding} from "./typesetters/atom";
 
 import type {Context} from "./types";
 import type {Scene} from "./scene-graph";
@@ -29,22 +30,6 @@ const typesetRow = (row: Editor.types.Row, context: Context): Layout.HBox => {
     }
 
     return box;
-};
-
-// TODO: check to see if there's an "=" in the previous column of any of the rows
-const withOperatorPadding = (
-    node: Layout.Node,
-    context: Context,
-): Layout.Node => {
-    const fontSize = fontSizeForContext(context);
-
-    // We need to tweak this loic so that we only add padding on the right side
-    // for binary operators below.  This is so that we don't get extra space
-    // when adding/subtracting something just to the right of an "=" in the above
-    return Layout.makeStaticHBox(
-        [Layout.makeKern(fontSize / 4), node, Layout.makeKern(fontSize / 4)],
-        context,
-    );
 };
 
 /**
@@ -254,32 +239,6 @@ const typesetFocus = (
     }
 };
 
-const _typesetAtom = (
-    node: Editor.types.Atom,
-    context: Context,
-): Layout.Glyph => {
-    const {font} = context.fontData;
-
-    const {value} = node;
-
-    const glyphID = font.getGlyphID(value.char);
-    let glyph = Layout.makeGlyph(value.char, glyphID, context);
-
-    // Convert individual glyphs to italic glyphs if they exist in the
-    // current font.
-    if (/[a-z]/.test(value.char) && !context.operator) {
-        const offset = value.char.charCodeAt(0) - "a".charCodeAt(0);
-        const char = String.fromCodePoint(0x1d44e + offset);
-        const glyphID = font.getGlyphID(char);
-        glyph = Layout.makeGlyph(char, glyphID, context);
-    }
-
-    glyph.id = node.id;
-    glyph.style = node.style;
-    glyph.pending = node.value.pending;
-    return glyph;
-};
-
 const typesetNode = (
     node: Editor.types.Node,
     context: Context,
@@ -357,87 +316,11 @@ const typesetNode = (
             return typesetTable(typesetChildren, node, context);
         }
         case "atom": {
-            return _typesetAtom(node, context);
+            return typesetAtom(node, context);
         }
         default:
             throw new UnreachableCaseError(node);
     }
-};
-
-const canBeUnary = (char: string): boolean => {
-    const unaryOperators = [
-        "+",
-        "\u2212", // \minus
-        "\u00B1", // \pm
-    ];
-
-    return unaryOperators.includes(char);
-};
-
-// TODO: dedupe with isOperator in slash.ts
-const isOperator = (char: string): boolean => {
-    // We don't include unary +/- in the numerator.  This mimic's mathquill's
-    // behavior.
-    const operators = [
-        "+",
-        "\u2212", // \minus
-        "\u00B1", // \pm
-        "\u00B7", // \times
-        "=",
-        "<",
-        ">",
-        "\u2260", // \neq
-        "\u2264", // \geq
-        "\u2265", // \leq
-    ];
-
-    if (operators.includes(char)) {
-        return true;
-    }
-
-    const charCode = char.charCodeAt(0);
-
-    // Arrows
-    if (charCode >= 0x2190 && charCode <= 0x21ff) {
-        return true;
-    }
-
-    return false;
-};
-
-const shouldHavePadding = (
-    prevNode: Editor.types.Node | Editor.Focus | undefined,
-    currentNode: Editor.types.Atom,
-    context: Context,
-): boolean => {
-    const currentChar = currentNode.value.char;
-
-    // We only add padding around operators, so if we get a non-operator char
-    // we can return early.
-    if (!isOperator(currentChar)) {
-        return false;
-    }
-
-    // No operators get padding when `cramped` is true
-    if (context.cramped) {
-        return false;
-    }
-
-    // If the currentChar can be unary we check a number of situations where it
-    // should be unary and don't give it any padding in those situations.
-    if (canBeUnary(currentChar)) {
-        if (
-            !prevNode ||
-            (prevNode.type === "atom" && isOperator(prevNode.value.char)) ||
-            prevNode.type === "limits" ||
-            prevNode.type === "zlimits"
-        ) {
-            return false;
-        }
-    }
-
-    // All other operators should have padding around them.
-    return true;
 };
 
 const typesetNodes = (
@@ -448,18 +331,7 @@ const typesetNodes = (
 ): readonly Layout.Node[] => {
     return nodes.map((child) => {
         if (child.type === "atom") {
-            const glyph = _typesetAtom(child, context);
-            const result = shouldHavePadding(prevChild, child, context)
-                ? withOperatorPadding(glyph, context)
-                : glyph;
-            if (result !== glyph) {
-                result.id = glyph.id;
-                delete glyph.id;
-                // Move the style to the result so that cancel overlays are
-                // continuous even when they include an operator with padding.
-                result.style = glyph.style;
-                glyph.style = {};
-            }
+            const result = maybeAddOperatorPadding(prevChild, child, context);
             prevLayoutNode = result;
             prevChild = child;
             return result;
