619bb131179cbfdeb31cca7c8f68435f186b28c1
Kevin Barabash <kevinb@khanacademy.org>
2021-08-21 19:30:38 -0400

    forgot to add the vite config file

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import path from "path";
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+export default defineConfig({
+    plugins: [reactRefresh()],
+    resolve: {
+        alias: [
+            { 
+                find: /^@math-blocks\/(.*)$/,
+                replacement: path.join(__dirname, 'packages/$1/src/index.ts'),
+            },
+        ],
+    },
+    publicDir: path.join(__dirname, "assets"),
+});
