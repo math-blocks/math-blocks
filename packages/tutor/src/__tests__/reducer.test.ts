6e4654bd32ebf18b2a9ca6b6012daa4f576bb2ea
Kevin Barabash <kevinb7@gmail.com>
2021-09-12 16:25:43 -0400

    Add tests to check that we return the same state object if the action shouldn't cause a mutation

--- packages/tutor/src/__tests__/reducer.test.ts
+++ packages/tutor/src/__tests__/reducer.test.ts
@@ -37,111 +37,186 @@ describe("reducer", () => {
         });
     });
 
-    it("should mark the last step as correct", () => {
-        const step: Step = {
-            status: StepStatus.Incorrect,
-            value: zipper,
-            mistakes: [],
-        };
+    describe("action: {type: 'right'}", () => {
+        it("should mark the last step as correct if it isn't already", () => {
+            const step: Step = {
+                status: StepStatus.Incorrect,
+                value: zipper,
+                mistakes: [],
+            };
 
-        const state: State = {
-            steps: [step],
-            status: ProblemStatus.Incomplete,
-        };
+            const state: State = {
+                steps: [step],
+                status: ProblemStatus.Incomplete,
+            };
 
-        const result = reducer(state, {type: "right", hint: "none"});
+            const result = reducer(state, {type: "right", hint: "none"});
 
-        expect(result).toEqual({
-            steps: [
-                {
-                    value: step.value,
-                    status: StepStatus.Correct,
-                    hint: "none",
-                },
-            ],
-            status: ProblemStatus.Incomplete,
+            expect(result).toEqual({
+                steps: [
+                    {
+                        value: step.value,
+                        status: StepStatus.Correct,
+                        hint: "none",
+                    },
+                ],
+                status: ProblemStatus.Incomplete,
+            });
+        });
+
+        it("should return the same object if the last step is already correct", () => {
+            const step: Step = {
+                status: StepStatus.Correct,
+                value: zipper,
+                hint: "none",
+            };
+
+            const state: State = {
+                steps: [step],
+                status: ProblemStatus.Incomplete,
+            };
+
+            const result = reducer(state, {type: "right", hint: "none"});
+
+            expect(result).toBe(state);
         });
     });
 
-    it("should mark the last step as incorrect", () => {
-        const step: Step = {
-            status: StepStatus.Incorrect,
-            value: zipper,
-            mistakes: [],
-        };
+    describe("action: {type: 'wrong'}", () => {
+        it("should mark the last step as incorrect", () => {
+            const step: Step = {
+                status: StepStatus.Correct,
+                value: zipper,
+                hint: "none",
+            };
 
-        const state: State = {
-            steps: [step],
-            status: ProblemStatus.Incomplete,
-        };
+            const state: State = {
+                steps: [step],
+                status: ProblemStatus.Incomplete,
+            };
 
-        const result = reducer(state, {type: "wrong", mistakes: []});
+            const result = reducer(state, {type: "wrong", mistakes: []});
 
-        expect(result).toEqual({
-            steps: [
-                {
-                    value: step.value,
-                    status: StepStatus.Incorrect,
-                    mistakes: [],
-                },
-            ],
-            status: ProblemStatus.Incomplete,
+            expect(result).toEqual({
+                steps: [
+                    {
+                        value: step.value,
+                        status: StepStatus.Incorrect,
+                        mistakes: [],
+                    },
+                ],
+                status: ProblemStatus.Incomplete,
+            });
+        });
+
+        it("should not return a new object if the last step is incorrect", () => {
+            const step: Step = {
+                status: StepStatus.Incorrect,
+                value: zipper,
+                mistakes: [],
+            };
+
+            const state: State = {
+                steps: [step],
+                status: ProblemStatus.Incomplete,
+            };
+
+            const result = reducer(state, {type: "wrong", mistakes: []});
+
+            expect(result).toBe(state);
         });
     });
 
-    it("should change the status of the last step to Pending", () => {
-        const step: Step = {
-            status: StepStatus.Incorrect,
-            value: zipper,
-            mistakes: [],
-        };
+    describe("action: {type: 'set_pending'}", () => {
+        it("should change the status of the last step to Pending", () => {
+            const step: Step = {
+                status: StepStatus.Incorrect,
+                value: zipper,
+                mistakes: [],
+            };
 
-        const state: State = {
-            steps: [step],
-            status: ProblemStatus.Incomplete,
-        };
+            const state: State = {
+                steps: [step],
+                status: ProblemStatus.Incomplete,
+            };
 
-        const result = reducer(state, {type: "set_pending"});
+            const result = reducer(state, {type: "set_pending"});
 
-        expect(result).toEqual({
-            steps: [
-                {
-                    value: step.value,
-                    status: StepStatus.Pending,
-                },
-            ],
-            status: ProblemStatus.Incomplete,
+            expect(result).toEqual({
+                steps: [
+                    {
+                        value: step.value,
+                        status: StepStatus.Pending,
+                    },
+                ],
+                status: ProblemStatus.Incomplete,
+            });
+        });
+
+        it("should return the same object if the last item is already pending", () => {
+            const step: Step = {
+                status: StepStatus.Pending,
+                value: zipper,
+            };
+
+            const state: State = {
+                steps: [step],
+                status: ProblemStatus.Incomplete,
+            };
+
+            const result = reducer(state, {type: "set_pending"});
+
+            expect(result).toBe(state);
         });
     });
 
-    it("should update the value of the last step", () => {
-        const step: Step = {
-            status: StepStatus.Incorrect,
-            value: zipper,
-            mistakes: [],
-        };
+    describe("action: {type: 'update'}", () => {
+        it("should update the value of the last step if the value is different", () => {
+            const step: Step = {
+                status: StepStatus.Incorrect,
+                value: zipper,
+                mistakes: [],
+            };
 
-        const state: State = {
-            steps: [step],
-            status: ProblemStatus.Incomplete,
-        };
+            const state: State = {
+                steps: [step],
+                status: ProblemStatus.Incomplete,
+            };
 
-        const newCharRow: Editor.types.CharRow = Editor.util.row("2x=5");
-        const newZipper = Editor.rowToZipper(newCharRow, []);
-        if (!newZipper) {
-            throw new Error("Couldn't create zipper");
-        }
+            const newCharRow: Editor.types.CharRow = Editor.util.row("2x=5");
+            const newZipper = Editor.rowToZipper(newCharRow, []);
+            if (!newZipper) {
+                throw new Error("Couldn't create zipper");
+            }
 
-        const result = reducer(state, {type: "update", value: newZipper});
+            const result = reducer(state, {type: "update", value: newZipper});
 
-        expect(result).toEqual({
-            steps: [
-                {
-                    ...step,
-                    value: newZipper,
-                },
-            ],
-            status: ProblemStatus.Incomplete,
+            expect(result).toEqual({
+                steps: [
+                    {
+                        ...step,
+                        value: newZipper,
+                    },
+                ],
+                status: ProblemStatus.Incomplete,
+            });
+        });
+
+        it("should return the same object if the value is the same object", () => {
+            const step: Step = {
+                status: StepStatus.Incorrect,
+                value: zipper,
+                mistakes: [],
+            };
+
+            const state: State = {
+                steps: [step],
+                status: ProblemStatus.Incomplete,
+            };
+
+            const result = reducer(state, {type: "update", value: zipper});
+
+            expect(result).toBe(state);
         });
     });
 
@@ -177,23 +252,42 @@ describe("reducer", () => {
         });
     });
 
-    it("should complete the problem", () => {
-        const step: Step = {
-            status: StepStatus.Incorrect,
-            value: zipper,
-            mistakes: [],
-        };
+    describe("action: {type: 'complete'}", () => {
+        it("should complete the problem", () => {
+            const step: Step = {
+                status: StepStatus.Incorrect,
+                value: zipper,
+                mistakes: [],
+            };
 
-        const state: State = {
-            steps: [step],
-            status: ProblemStatus.Incomplete,
-        };
+            const state: State = {
+                steps: [step],
+                status: ProblemStatus.Incomplete,
+            };
 
-        const result = reducer(state, {type: "complete"});
+            const result = reducer(state, {type: "complete"});
 
-        expect(result).toEqual({
-            steps: state.steps,
-            status: ProblemStatus.Complete,
+            expect(result).toEqual({
+                steps: state.steps,
+                status: ProblemStatus.Complete,
+            });
+        });
+
+        it("should return the same object if already complete", () => {
+            const step: Step = {
+                status: StepStatus.Correct,
+                value: zipper,
+                hint: "none",
+            };
+
+            const state: State = {
+                steps: [step],
+                status: ProblemStatus.Complete,
+            };
+
+            const result = reducer(state, {type: "complete"});
+
+            expect(result).toBe(state);
         });
     });
 
