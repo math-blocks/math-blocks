a32075c576217c3094bc373674aab65410e8578e
Kevin Barabash <kevinb7@gmail.com>
2021-09-19 19:40:06 -0400

    Introduce  type and  function

--- packages/solver/src/types.ts
+++ packages/solver/src/types.ts
@@ -6,3 +6,21 @@ export type Step<T extends Semantic.types.Node = Semantic.types.Node> = {
     readonly after: T;
     readonly substeps: readonly Step<T>[];
 };
+
+export type Solution<T extends Semantic.types.Node = Semantic.types.Node> = {
+    readonly steps: readonly Step<Semantic.types.Node>[];
+    readonly answer: T;
+};
+
+type SolveEquation = {
+    readonly type: "SolveEquation";
+    readonly equation: Semantic.types.Eq;
+    readonly variable: Semantic.types.Identifier;
+};
+
+type SimplifyExpression = {
+    readonly type: "SimplifyExpression";
+    readonly expression: Semantic.types.NumericNode;
+};
+
+export type Problem = SolveEquation | SimplifyExpression;
