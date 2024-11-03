2423339a407314e6c59a6800f623363527b8d3c4
Kevin Barabash <kevinb7@gmail.com>
2024-11-03 16:09:09 -0500

    Add support for vertical navigation to the simple editor (#550)
    * Add support for vertical navigation to the simple editor
    
    * add unit tests
    
    * move test file into the correct folder

---
+++ packages/editor/src/simple-reducer/reducers/__tests__/move-vertically.test.ts
@@ -0,0 +1,510 @@
+import * as builders from '../../../char/builders';
+import { State } from '../../types';
+import { moveVertically } from '../move-vertically';
+
+type Cursor = {
+  readonly offset: number;
+  readonly path: readonly number[];
+};
+
+describe('moveVertically', () => {
+  describe('frac', () => {
+    it('should move down from numerator to denominator in a fraction', () => {
+      const row = builders.row([
+        builders.frac([builders.char('a')], [builders.char('b')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 0,
+        path: [0, 0],
+      };
+      const state: State = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowDown' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 0,
+        path: [0, 1],
+      });
+    });
+
+    it('should move up from denominator to numerator in a fraction', () => {
+      const row = builders.row([
+        builders.frac([builders.char('a')], [builders.char('b')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 0,
+        path: [0, 1],
+      };
+      const state: State = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowUp' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 0,
+        path: [0, 0],
+      });
+    });
+
+    it('should use the original offset when moving down', () => {
+      const row = builders.row([
+        builders.frac([builders.char('a')], [builders.char('b')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 1,
+        path: [0, 0],
+      };
+      const state: State = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowDown' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 1,
+        path: [0, 1],
+      });
+    });
+
+    it('should use the original offset when moving up', () => {
+      const row = builders.row([
+        builders.frac([builders.char('a')], [builders.char('b')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 1,
+        path: [0, 1],
+      };
+      const state: State = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowUp' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 1,
+        path: [0, 0],
+      });
+    });
+
+    it('should not change state if it cannot move down', () => {
+      const row = builders.row([
+        builders.frac([builders.char('a')], [builders.char('b')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 1,
+        path: [0, 1],
+      };
+      const state: State = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowDown' });
+
+      expect(newState).toBe(state);
+    });
+
+    it('should not change state if it cannot move up', () => {
+      const row = builders.row([
+        builders.frac([builders.char('a')], [builders.char('b')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 1,
+        path: [0, 0],
+      };
+      const state: State = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowUp' });
+
+      expect(newState).toBe(state);
+    });
+  });
+
+  describe('table (matrix)', () => {
+    it('should move down in a matrix', () => {
+      const row = builders.row([
+        builders.matrix(
+          [
+            [
+              builders.row([builders.char('a')]),
+              builders.row([builders.char('b')]),
+            ],
+            [
+              builders.row([builders.char('c')]),
+              builders.row([builders.char('d')]),
+            ],
+          ],
+          2,
+          2,
+        ),
+      ]);
+      const cursor: Cursor = {
+        offset: 0,
+        path: [0, 0],
+      };
+      const state = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowDown' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 0,
+        path: [0, 2],
+      });
+    });
+
+    it('should move up in a matrix', () => {
+      const row = builders.row([
+        builders.matrix(
+          [
+            [
+              builders.row([builders.char('a')]),
+              builders.row([builders.char('b')]),
+            ],
+            [
+              builders.row([builders.char('c')]),
+              builders.row([builders.char('d')]),
+            ],
+          ],
+          2,
+          2,
+        ),
+      ]);
+      const cursor: Cursor = {
+        offset: 0,
+        path: [0, 2],
+      };
+      const state = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowUp' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 0,
+        path: [0, 0],
+      });
+    });
+
+    it('should not move down if the cursor is in the bottom row', () => {
+      const row = builders.row([
+        builders.matrix(
+          [
+            [
+              builders.row([builders.char('a')]),
+              builders.row([builders.char('b')]),
+            ],
+            [
+              builders.row([builders.char('c')]),
+              builders.row([builders.char('d')]),
+            ],
+          ],
+          2,
+          2,
+        ),
+      ]);
+      const cursor: Cursor = {
+        offset: 0,
+        path: [0, 2],
+      };
+      const state = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowDown' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 0,
+        path: [0, 2],
+      });
+    });
+
+    it('should not move up if the cursor is in the top row', () => {
+      const row = builders.row([
+        builders.matrix(
+          [
+            [
+              builders.row([builders.char('a')]),
+              builders.row([builders.char('b')]),
+            ],
+            [
+              builders.row([builders.char('c')]),
+              builders.row([builders.char('d')]),
+            ],
+          ],
+          2,
+          2,
+        ),
+      ]);
+      const cursor: Cursor = {
+        offset: 0,
+        path: [0, 0],
+      };
+      const state = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowUp' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 0,
+        path: [0, 0],
+      });
+    });
+  });
+
+  describe('sub', () => {
+    it('moving up should place the cursor before the sub', () => {
+      const row = builders.row([
+        builders.row([builders.char('x')]),
+        builders.subsup([builders.char('n')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 0,
+        path: [1, 0],
+      };
+      const state = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowUp' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 1,
+        path: [],
+      });
+    });
+
+    it('moving up should place the cursor after the sub', () => {
+      const row = builders.row([
+        builders.row([builders.char('x')]),
+        builders.subsup([builders.char('n')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 1,
+        path: [1, 0],
+      };
+      const state = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowUp' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 2,
+        path: [],
+      });
+    });
+
+    it('should do nothing if you cannot move down', () => {
+      const row = builders.row([
+        builders.row([builders.char('x')]),
+        builders.subsup([builders.char('n')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 0,
+        path: [1, 0],
+      };
+      const state = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowDown' });
+
+      expect(newState).toBe(state);
+    });
+  });
+
+  describe('sup', () => {
+    it('moving down should place the cursor before the sub', () => {
+      const row = builders.row([
+        builders.row([builders.char('x')]),
+        builders.subsup(undefined, [builders.char('2')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 0,
+        path: [1, 1],
+      };
+      const state = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowDown' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 1,
+        path: [],
+      });
+    });
+
+    it('moving down should place the cursor after the sub', () => {
+      const row = builders.row([
+        builders.row([builders.char('x')]),
+        builders.subsup(undefined, [builders.char('2')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 1,
+        path: [1, 1],
+      };
+      const state = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowDown' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 2,
+        path: [],
+      });
+    });
+
+    it('should do nothing if you cannot move up', () => {
+      const row = builders.row([
+        builders.row([builders.char('x')]),
+        builders.subsup(undefined, [builders.char('2')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 0,
+        path: [1, 1],
+      };
+      const state = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowUp' });
+
+      expect(newState).toBe(state);
+    });
+  });
+
+  describe('subsup', () => {
+    it('moving down should move the cursor to the start of the sub', () => {
+      const row = builders.row([
+        builders.row([builders.char('x')]),
+        builders.subsup([builders.char('n')], [builders.char('2')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 0,
+        path: [1, 1],
+      };
+      const state = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowDown' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 0,
+        path: [1, 0],
+      });
+    });
+
+    it('moving down should place the cursor at the end of the sub', () => {
+      const row = builders.row([
+        builders.row([builders.char('x')]),
+        builders.subsup([builders.char('n')], [builders.char('2')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 1,
+        path: [1, 1],
+      };
+      const state = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowDown' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 1,
+        path: [1, 0],
+      });
+    });
+
+    it('moving up should move the cursor to the start of the sup', () => {
+      const row = builders.row([
+        builders.row([builders.char('x')]),
+        builders.subsup([builders.char('n')], [builders.char('2')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 0,
+        path: [1, 0],
+      };
+      const state = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowUp' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 0,
+        path: [1, 1],
+      });
+    });
+
+    it('moving up should place the cursor at the end of the sup', () => {
+      const row = builders.row([
+        builders.row([builders.char('x')]),
+        builders.subsup([builders.char('n')], [builders.char('2')]),
+      ]);
+      const cursor: Cursor = {
+        offset: 1,
+        path: [1, 0],
+      };
+      const state = {
+        row,
+        selection: { anchor: cursor, focus: cursor },
+        selecting: false,
+      };
+
+      const newState = moveVertically(state, { type: 'ArrowUp' });
+
+      expect(newState.selection.focus).toEqual<Cursor>({
+        offset: 1,
+        path: [1, 1],
+      });
+    });
+  });
+});
--- packages/editor/src/simple-reducer/reducers/move-vertically.ts
+++ packages/editor/src/simple-reducer/reducers/move-vertically.ts
@@ -1,6 +1,135 @@
+import * as PathUtils from '../path-utils';
 import type { State } from '../types';
 import type { Action } from '../../reducer/action-types';
+import type { Path, Selection } from '../types';
+import type { CharRow } from '../../char/types';
 
 export const moveVertically = (state: State, action: Action): State => {
+  const { row, selection } = state;
+
+  const { offset, path } = selection.focus;
+  const newSelection = getNewSelection(row, offset, path, action);
+
+  if (newSelection) {
+    return {
+      ...state,
+      selection: newSelection,
+    };
+  }
+
   return state;
+};
+
+const getNewSelection = (
+  root: CharRow,
+  offset: number,
+  path: Path,
+  action: Action,
+): Selection | undefined => {
+  if (path.length === 0) {
+    return undefined;
+  }
+
+  const parent = PathUtils.getNodeAtPath(root, path.slice(0, -1));
+  const parentOffset = path[path.length - 1];
+
+  switch (parent?.type) {
+    case 'table': {
+      const rowCount = parent.rowCount;
+      const colCount = parent.colCount;
+      const row = Math.floor(parentOffset / colCount);
+      const col = parentOffset % colCount;
+
+      let newRow = row;
+      if (action.type === 'ArrowDown') {
+        newRow++;
+      } else if (action.type === 'ArrowUp') {
+        newRow--;
+      }
+
+      if (newRow === row || newRow < 0 || newRow >= rowCount) {
+        break;
+      }
+
+      const newParentOffset = newRow * colCount + col;
+      const newElement = parent.children[newParentOffset];
+      const newFocus = {
+        path: [...path.slice(0, -1), newParentOffset],
+        offset: newElement ? Math.min(offset, newElement.children.length) : 0,
+      };
+
+      return {
+        anchor: newFocus,
+        focus: newFocus,
+      };
+    }
+    case 'frac': {
+      let newParentOffset = parentOffset;
+      if (newParentOffset === 0 && action.type === 'ArrowDown') {
+        newParentOffset = 1;
+      } else if (newParentOffset === 1 && action.type === 'ArrowUp') {
+        newParentOffset = 0;
+      }
+
+      if (newParentOffset === parentOffset) {
+        break;
+      }
+
+      const newElement = parent.children[newParentOffset];
+      const newFocus = {
+        path: [...path.slice(0, -1), newParentOffset],
+        offset: newElement ? Math.min(offset, newElement.children.length) : 0,
+      };
+
+      return {
+        anchor: newFocus,
+        focus: newFocus,
+      };
+    }
+    case 'subsup': {
+      let newParentOffset = parentOffset;
+      if (newParentOffset === 1 && action.type === 'ArrowDown') {
+        newParentOffset = 0;
+      } else if (newParentOffset === 0 && action.type === 'ArrowUp') {
+        newParentOffset = 1;
+      }
+
+      if (newParentOffset === parentOffset) {
+        break;
+      }
+
+      const newElement = parent.children[newParentOffset];
+      const newFocus = newElement
+        ? {
+            // If the sup or sub has a matching sub or sup, move the cursor to the
+            // corresponding sub or sup.
+            path: [...path.slice(0, -1), newParentOffset],
+            offset: newElement
+              ? Math.min(offset, newElement.children.length)
+              : 0,
+          }
+        : {
+            path: [...path.slice(0, -2)],
+            offset:
+              // If we're at the end of the sup or sub and there is no corresponding
+              // sub or sup, move the cursor to appear after the subsup in the parent
+              // row.
+              offset === parent.children[parentOffset]!.children.length
+                ? path[path.length - 2] + 1
+                : path[path.length - 2],
+          };
+
+      return {
+        anchor: newFocus,
+        focus: newFocus,
+      };
+    }
+    case 'limits': {
+      /* istanbul ignore next */
+      throw new Error('Move Vertically - subsup - Not implemented');
+    }
+  }
+
+  // Recurses up the tree to find a parent that can be navigated to.
+  return getNewSelection(root, parentOffset, path.slice(0, -1), action);
 };
