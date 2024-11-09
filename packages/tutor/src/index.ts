a6bb3719bf125ca8fc2881d7cbc7cd562aa0defd
Kevin Barabash <kevinb7@gmail.com>
2024-11-09 17:03:41 -0500

    remove tutor package

--- packages/tutor/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './grader/index';
-export * from './help/highlighting';
-export { getHint } from './help/get-hint';
-export { showMeHow } from './help/show-me-how';
-
-export * from './session/reducer';
-export { createInitialState } from './session/create-initial-state';
