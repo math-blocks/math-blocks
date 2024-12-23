96dce5eb29a05736ee928175580fb0252452bc6b
Kevin Barabash <kevinb7@gmail.com>
2024-12-22 19:33:46 -0500

    Update solver page to support factoring and solving quadratic equations (#599)
    * Update solver page to support factoring and solving quadratic equations
    
    * tweak the vertical alignment of MathRenderer

--- demo/src/solver/solver-page.tsx
+++ demo/src/solver/solver-page.tsx
@@ -5,6 +5,7 @@ import * as Editor from '@math-blocks/editor';
 import * as Semantic from '@math-blocks/semantic';
 import * as Solver from '@math-blocks/solver';
 import * as Typesetter from '@math-blocks/typesetter';
+import * as Tex from '@math-blocks/tex';
 import { MathEditor, MathRenderer, FontDataContext } from '@math-blocks/react';
 import { getFontData, parse } from '@math-blocks/opentype';
 import type { Font } from '@math-blocks/opentype';
@@ -16,18 +17,10 @@ import Substeps from './substeps';
 
 const operators = Object.keys(macros).filter((key) => key === macros[key]);
 
-const b = Editor.builders;
-const question: Editor.types.CharRow = b.row([
-  b.frac(
-    [
-      b.char('x'),
-      b.subsup(undefined, [b.char('2')]),
-      b.char('x'),
-      b.subsup(undefined, [b.char('4')]),
-    ],
-    [b.char('x'), b.subsup(undefined, [b.char('3')])],
-  ),
-]);
+// const parser = new Tex.Parser('x^2 + 5x + 6 = 0');
+// TODO: Update the TeX parser to convert dashes to the minus sign
+const parser = new Tex.Parser('2x + 3y \u2212 7 = x \u2212 y + 1');
+const question: Editor.types.CharRow = parser.parse();
 
 const safeParse = (input: Editor.types.CharRow): Semantic.types.Node | null => {
   try {
@@ -38,66 +31,106 @@ const safeParse = (input: Editor.types.CharRow): Semantic.types.Node | null => {
 };
 
 // TODO:
-// - show error messages in the UI
 // - provide a UI disclosing sub-steps
 // - use the colorMap option to highlight related nodes between steps
 //   e.g. 2(x + y) -> 2x + 2y the 2s would be the same color, etc.
-// - update MathRenderer to do the typesetting
 
 const SolverPage: React.FunctionComponent = () => {
   const [ast, setAst] = React.useState(safeParse(question));
   const [answer, setAnswer] = React.useState<Editor.types.CharRow | null>(null);
   const [step, setStep] = React.useState<Solver.Step | null>(null);
   const [error, setError] = React.useState<string | null>(null);
+  const [action, setAction] = React.useState<Solver.Problem['type']>(
+    'SolveLinearRelation',
+  );
 
-  const handleSimplify = React.useCallback((): void => {
+  const handleGo = React.useCallback(() => {
     if (!ast) {
       setError("Couldn't parse input");
       return;
     }
-    if (!Semantic.util.isNumeric(ast)) {
-      setError('ast is not a NumericNode');
-      return;
-    }
-    const problem: Solver.Problem = {
-      type: 'SimplifyExpression',
-      expression: ast,
-    };
-    const result = Solver.solveProblem(problem);
-    if (result) {
-      console.log(result);
-      setAnswer(Editor.print(result.answer));
-      setStep(result.steps[0]);
-      setError(null);
-    } else {
-      setError('no solution found');
-    }
-  }, [ast]);
-
-  const handleSolve = React.useCallback((): void => {
-    if (!ast) {
-      setError("Couldn't parse input");
-      return;
-    }
-    if (Semantic.util.isNumericRelation(ast)) {
-      const problem: Solver.Problem = {
-        type: 'SolveLinearRelation',
-        relation: ast,
-        variable: Semantic.builders.identifier('x'),
-      };
-      const result = Solver.solveProblem(problem);
-      if (result) {
-        console.log(result);
+    switch (action) {
+      case 'Factor': {
+        if (ast.type !== 'Add') {
+          setError('ast is not an Add node');
+          return;
+        }
+        const problem: Solver.Problem = {
+          type: 'Factor',
+          expression: ast,
+        };
+        const result = Solver.solveProblem(problem);
+        if (result) {
+          console.log(result);
+          setAnswer(Editor.print(result.answer));
+          setStep(result.steps[0]);
+          setError(null);
+        } else {
+          setError('Expression could not be factored');
+        }
+        break;
+      }
+      case 'SimplifyExpression': {
+        if (!Semantic.util.isNumeric(ast)) {
+          setError('ast is not a NumericNode');
+          return;
+        }
+        const problem: Solver.Problem = {
+          type: 'SimplifyExpression',
+          expression: ast,
+        };
+        const result = Solver.solveProblem(problem);
+        if (!result) {
+          setError('no solution found');
+          return;
+        }
         setAnswer(Editor.print(result.answer));
         setStep(result.steps[0]);
         setError(null);
-      } else {
-        setError('no solution found');
+        break;
       }
-    } else {
-      setError("can't solve something that isn't an equation");
+      case 'SolveLinearRelation': {
+        if (!Semantic.util.isNumericRelation(ast)) {
+          setError("can't solve something that isn't an relation");
+          return;
+        }
+        const problem: Solver.Problem = {
+          type: 'SolveLinearRelation',
+          relation: ast,
+          variable: Semantic.builders.identifier('x'),
+        };
+        const result = Solver.solveProblem(problem);
+        if (!result) {
+          setError('no solution found');
+          return;
+        }
+        setAnswer(Editor.print(result.answer));
+        setStep(result.steps[0]);
+        setError(null);
+        break;
+      }
+      case 'SolveQuadraticEquation': {
+        if (ast.type !== 'Equals') {
+          setError("can't solve something that isn't an equation");
+          return;
+        }
+        const problem: Solver.Problem = {
+          type: 'SolveQuadraticEquation',
+          relation: ast,
+          variable: Semantic.builders.identifier('x'),
+        };
+        const result = Solver.solveProblem(problem);
+        if (!result) {
+          setError('no solution found');
+          return;
+        }
+        setAnswer(Editor.print(result.answer));
+        setStep(result.steps[0]);
+        setError(null);
+        break;
+      }
     }
-  }, [ast]);
+  }, [action, ast]);
 
   const [font, setFont] = React.useState<Font | null>(null);
 
@@ -117,7 +150,7 @@ const SolverPage: React.FunctionComponent = () => {
     return null;
   }
 
-  const fontSize = 64;
+  const fontSize = 24;
   const context: Typesetter.Context = {
     fontData: getFontData(font, 'STIX2'),
     baseFontSize: fontSize,
@@ -130,51 +163,69 @@ const SolverPage: React.FunctionComponent = () => {
 
   const maybeRenderSolution = (): React.ReactNode => {
     if (answer != null) {
-      return <MathRenderer row={answer} />;
+      return <MathRenderer row={answer} fontSize={24} />;
     }
     return null;
   };
 
   const showSolution = answer != null && !error;
 
-  const canSimplify = ast && Semantic.util.isNumeric(ast);
-  const canSolve = ast && Semantic.util.isNumericRelation(ast);
-
   return (
     <FontDataContext.Provider value={context.fontData}>
-      <div style={styles.container}>
-        <div>
-          <div style={styles.label}>Question:</div>
-          {canSimplify && <button onClick={handleSimplify}>Simplify</button>}
-          {canSolve && <button onClick={handleSolve}>Solve</button>}
+      <div style={styles.outer}>
+        <div style={styles.inner}>
+          <h1 style={{ fontFamily: 'sans-serif' }}>Math Blocks: Solver</h1>
+          <div>
+            <MathEditor
+              readonly={false}
+              row={question}
+              onChange={(state) => setAst(safeParse(state.row))}
+              style={{ minWidth: '100%' }}
+              fontSize={24}
+            />
+          </div>
+          <div style={{ marginTop: 8 }}>
+            <select
+              style={{ marginRight: 8 }}
+              value={action}
+              onChange={(event) =>
+                setAction(event.target.value as Solver.Problem['type'])
+              }
+            >
+              <option value="Factor">Factor</option>
+              <option value="SimplifyExpression">Simplify Expression</option>
+              <option value="SolveLinearRelation">Solve Linear Relation</option>
+              <option value="SolveQuadraticEquation">
+                Solve Quadratic Equation
+              </option>
+            </select>
+            <button onClick={handleGo}>Go</button>
+          </div>
+          <div style={styles.gap}></div>
+          {error && <h1>Error</h1>}
+          {error && <h1>{error}</h1>}
+          {showSolution && <div style={styles.label}>Steps:</div>}
+          {showSolution && step && <Substeps start={step.before} step={step} />}
+          {showSolution && <div style={styles.label}>Answer:</div>}
+          {showSolution && maybeRenderSolution()}
         </div>
-        <div>
-          <MathEditor
-            readonly={false}
-            row={question}
-            onChange={(state) => setAst(safeParse(state.row))}
-          />
-        </div>
-        <div style={styles.gap}></div>
-        <div style={styles.gap}></div>
-        {error && <h1>Error</h1>}
-        {error && <h1>{error}</h1>}
-        {showSolution && <div style={styles.label}>Steps:</div>}
-        {showSolution && step && <Substeps start={step.before} step={step} />}
-        {showSolution && <div style={styles.label}>Answer:</div>}
-        {showSolution && maybeRenderSolution()}
       </div>
     </FontDataContext.Provider>
   );
 };
 
 const styles: Record<string, React.CSSProperties> = {
-  container: {
-    display: 'grid',
-    gridTemplateColumns: '200px auto',
+  outer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  inner: {
+    width: 800,
   },
   label: {
     paddingTop: 16,
+    paddingBottom: 12,
     fontSize: 32,
     fontFamily: 'sans-serif',
   },
--- demo/src/solver/substeps.tsx
+++ demo/src/solver/substeps.tsx
@@ -4,6 +4,7 @@ import * as Editor from '@math-blocks/editor';
 import * as Semantic from '@math-blocks/semantic';
 import * as Solver from '@math-blocks/solver';
 import { MathRenderer } from '@math-blocks/react';
+import { MathStyle } from '@math-blocks/typesetter';
 
 type Props = {
   // Prefix to start numbering from, e.g. 1.2.3
@@ -16,35 +17,40 @@ type Props = {
 };
 
 const Substeps: React.FunctionComponent<Props> = ({ prefix, start, step }) => {
-  let current = start;
-
-  const beforeRow = Editor.print(step.before);
+  const marginBottom = 8;
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <MathRenderer row={beforeRow} style={{ marginBottom: 32 }} />
       {step.substeps.map((substep, index) => {
-        const before = current;
-
-        const after = Solver.applyStep(before, substep);
+        const before = substep.before;
+        const beforeRow = Editor.print(substep.before);
         const afterRow = Editor.print(substep.after);
-
-        current = after;
 
         const num = prefix ? `${prefix}.${index + 1}` : `${index + 1}`;
 
         return (
           <React.Fragment key={index + 1}>
-            <div>
-              {num}: {substep.message}
+            <div style={{ paddingBottom: 4, fontFamily: 'sans-serif' }}>
+              {num}: {printStep(substep)}
             </div>
+            <MathRenderer
+              row={beforeRow}
+              style={{ marginBottom: marginBottom }}
+              fontSize={24}
+            />
             {/* TODO: special case substeps.length === 1 */}
             {substep.substeps.length > 0 && (
               <div style={{ paddingLeft: 64 }}>
                 <Substeps prefix={num} start={before} step={substep} />
               </div>
             )}
-            {<MathRenderer row={afterRow} style={{ marginBottom: 32 }} />}
+            {
+              <MathRenderer
+                row={afterRow}
+                style={{ marginBottom: marginBottom }}
+                fontSize={24}
+              />
+            }
           </React.Fragment>
         );
       })}
@@ -53,3 +59,39 @@ const Substeps: React.FunctionComponent<Props> = ({ prefix, start, step }) => {
 };
 
 export default Substeps;
+
+const printStep = (step: Solver.Step): React.ReactNode => {
+  switch (step.message) {
+    case 'do the same operation to both sides': {
+      const { operation, value } = step;
+      const renderedValue = (
+        <MathRenderer
+          row={Editor.print(value)}
+          fontSize={18}
+          mathStyle={MathStyle.Display}
+        />
+      );
+      switch (operation) {
+        case 'add': {
+          return <span>Add {renderedValue} from both sides</span>;
+        }
+        case 'sub': {
+          return <span>Subtract {renderedValue} from both sides</span>;
+        }
+        case 'mul': {
+          return <span>Multiply both sides by {renderedValue}</span>;
+        }
+        case 'div': {
+          return <span>Divide both sides by {renderedValue}</span>;
+        }
+      }
+      break;
+    }
+    case 'move matching variable terms to one side': {
+      const { side } = step;
+      return <span>Move matching variable terms to the {side} side</span>;
+    }
+    default:
+      return <span>{step.message}</span>;
+  }
+};
