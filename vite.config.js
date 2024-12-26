1335fbcf3995cbd4f3dceda29edbb6a2b2c283e5
Kevin Barabash <kevinb7@gmail.com>
2024-12-26 12:04:14 -0500

    create a simple vitejs plugin to replace the base href when deploying

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,23 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const updateBasePlugin = () => {
+  return {
+    name: 'update-base',
+    transformIndexHtml(html, ctx) {
+      if (ctx.bundle) {
+        return html.replace(
+          '<base href="/" />',
+          '<base href="/math-blocks/" />',
+        );
+      }
+      return html;
+    },
+  };
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), updateBasePlugin()],
   build: {
     sourcemap: true,
   },
